65ac18b52f4e7e046730cd60b3bc5099a418e234
johnnyboycurtis <johnnyboycurtis@gmail.com>
2025-07-10 22:32:28 -0700

    Add setup script

---
+++ ModernBERT/setup.sh
@@ -0,0 +1,9 @@
+conda create -n torch python=3.11 -y
+
+conda activate torch
+
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
+pip install --upgrade "sentence-transformers[onnx-gpu]" transformers panda tqdm ipython jupyter notebook ipykernel accelerate
+
+conda install -c nvidia cuda-toolkit -y
+pip install flash-attn --no-build-isolation
